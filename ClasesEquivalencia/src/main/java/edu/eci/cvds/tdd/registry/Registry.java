4d7ae10746139e4afc5b0332c6a85196329d4149
unknown <henry.sanchez@labinfo.is.escuelaing.edu.co>
2020-02-01 09:17:15 -0500

    Lab03

---
+++ ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
@@ -0,0 +1,25 @@
+package edu.eci.cvds.tdd.registry;
+
+public class Registry {
+	public RegisterResult registerVoter(Person p) {
+		RegisterResult res;
+		if (p.getAge()<18) {
+			res=RegisterResult.UNDERAGE;
+		}
+		else if (p.getAge()<0 || p.getAge()>125) {
+			res=RegisterResult.INVALID_AGE;
+		}
+		else if (!p.isAlive()) {
+			res=RegisterResult.DEAD;
+		}
+		else if(p.getMil()) {
+			res=RegisterResult.MILITAR;
+		}
+		else {
+			res=RegisterResult.VALID;
+		}
+		
+		
+        return res;
+    }
+}
---
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
@@ -0,0 +1,52 @@
+package edu.eci.cvds.tdd.registry;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class RegistryTest {
+	public RegistryTest(){
+		super();
+	}
+
+    private Registry registry = new Registry();
+
+    @Test
+    public void Bien() {
+    	Person prueba = new Person("Henry",154616,25,Gender.MALE,true,false);
+    	 RegisterResult result = registry.registerVoter(prueba);
+    	Assert.assertEquals(RegisterResult.VALID, result);
+    }
+    
+    @Test
+    public void Militar() {
+    	Person prueba = new Person("Henry",154616,25,Gender.MALE,true,true);
+    	 RegisterResult result = registry.registerVoter(prueba);
+    	Assert.assertEquals(RegisterResult.MILITAR, result);
+    }
+    
+    @Test
+    public void MenorEdad() {
+    	Person prueba = new Person("Henry",154616,15,Gender.MALE,true,false);
+    	 RegisterResult result = registry.registerVoter(prueba);
+    	Assert.assertEquals(RegisterResult.UNDERAGE, result);
+    }
+    
+    @Test
+    public void edadInvalida() {
+    	Person prueba = new Person("Henry",154616,500,Gender.MALE,true,false);
+    	 RegisterResult result = registry.registerVoter(prueba);
+    	Assert.assertEquals(RegisterResult.INVALID_AGE, result);
+    }
+    
+    @Test
+    public void Muerto() {
+    	Person prueba = new Person("Henry",154616,25,Gender.MALE,false,false);
+    	 RegisterResult result = registry.registerVoter(prueba);
+    	Assert.assertEquals(RegisterResult.DEAD, result);
+    }
+
+    // TODO Complete with more test cases
+}
