e5c63c1e4269b22f1a762172de0972014e1a646a
unknown <edwin.rodriguez-m@labinfo.is.escuelaing.edu.co>
2020-02-07 17:42:16 -0500

    corregido

--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
@@ -6,13 +6,17 @@ import org.junit.Test;
 
 public class TarifasTest {
 	private CalculadorDescuentos calcu = new CalculadorDescuentos();
-	
+	/**
+	 * La prueba debe dar 200 ya que la tarifa es 200 y no tiene descuento
+	 */
 	@Test
 	public void deberiaSerValidoSinDescuento() {
 		Assert.assertEquals(200.0,(calcu.calculoTarifa(200,1,18)),0.0);
 		Assert.assertEquals(200.0,(calcu.calculoTarifa(200,20,65)),0.0);
 	}
-	
+	/**
+	 * La prueba debe dar un descuento de 5%
+	 */
 	@Test
 	public void deberiaValidoDescuentoEdadMenor(){
 		Assert.assertEquals(200.0,(calcu.calculoTarifa(200, 20,17)),0.0);
@@ -45,20 +49,20 @@ public class TarifasTest {
 	
 	@Test
 	public void deberiaSerInvalidoPorEdad() {
-		Assert.assertEquals(160.0,(calcu.calculoTarifa(200,21,-1)),0.0);
-		Assert.assertEquals(160.0,(calcu.calculoTarifa(200,31,0)),0.0);
-		Assert.assertEquals(154.0,(calcu.calculoTarifa(200,21,123)),0.0);
+		Assert.assertNotEquals(0.0,(calcu.calculoTarifa(200,21,-1)),0.0);
+		Assert.assertNotEquals(0.0,(calcu.calculoTarifa(200,31,0)),0.0);
+		Assert.assertNotEquals(0.0,(calcu.calculoTarifa(200,21,123)),0.0);
 	}
 	
 	@Test
 	public void deberiaSerInvalidoPorTarifa() {
-		Assert.assertEquals(-0.95,(calcu.calculoTarifa(-1,1,1)),0.0);
-		Assert.assertEquals(0.0,(calcu.calculoTarifa(0,1,1)),0.0);
+		Assert.assertNotEquals(100.0,(calcu.calculoTarifa(-1,1,1)),0.0);
+		Assert.assertNotEquals(100.0,(calcu.calculoTarifa(0,1,1)),0.0);
 	}
 	
 	@Test
 	public void deberiaSerInvalidoPorDias() {
-		Assert.assertEquals(190.0,(calcu.calculoTarifa(200,-1,1)),0.0);
-		Assert.assertEquals(190.0,(calcu.calculoTarifa(200,0,1)),0.0);
+		Assert.assertNotEquals(0.0,(calcu.calculoTarifa(200,-1,1)),0.0);
+		Assert.assertNotEquals(0.0,(calcu.calculoTarifa(200,0,1)),0.0);
 	}
 }
--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
@@ -13,13 +13,19 @@ public class RegistryTest {
 
     private Registry registry = new Registry();
     
-    @Test
-    public void Bien() {
+    /**
+     * La prueba reciber valores validos y la persona puede votar
+     */
+    @Test 
+    public void deberiaSerValido() {
     	Person prueba = new Person("Henry",154616,25,Gender.MALE,true,false);
     	 RegisterResult result = registry.registerVoter(prueba);
     	Assert.assertEquals(RegisterResult.VALID, result);
     }
     
+    /**
+     * La prueba debe dar que la persona es militar(el militar no puede votar)
+     */
     @Test
     public void Militar() {
     	Person prueba = new Person("Henry",154616,25,Gender.MALE,true,true);
@@ -27,6 +33,9 @@ public class RegistryTest {
     	Assert.assertEquals(RegisterResult.MILITAR, result);
     }
     
+    /**
+     * La prueba debe dar que la persona es menor de edad y por lo tanto no puede votar
+     */
     @Test
     public void MenorEdad() {
     	Person prueba = new Person("Henry",154616,15,Gender.MALE,true,false);
@@ -34,6 +43,9 @@ public class RegistryTest {
     	Assert.assertEquals(RegisterResult.UNDERAGE, result);
     }
     
+    /**
+     * La prueba debe dar que la edad es invalida porque en este caso es 500
+     */
     @Test
     public void edadInvalida() {
     	Person prueba = new Person("Henry",154616,500,Gender.MALE,true,false);
@@ -41,13 +53,19 @@ public class RegistryTest {
     	Assert.assertEquals(RegisterResult.INVALID_AGE, result);
     }
     
+    /**
+     * La prueba debe dar que el la edad no es validad porque tiene 123 años
+     */
     @Test
-    public void Rango() {
-    	Person prueba = new Person("Henry",1546163,123,Gender.MALE,true,false);
+    public void RangoEdadMayorNoAceptada() {
+    	Person prueba = new Person("Henryfe",1546163,123,Gender.MALE,true,false);
     	 RegisterResult result = registry.registerVoter(prueba);
     	Assert.assertEquals(RegisterResult.INVALID_AGE, result);
     }
     
+    /**
+     * La prueba debe debe dar valido ya que su edad es 122 que es la maxima aceptada
+     */
     @Test
     public void RangoEdadMayorAceptada() {
     	Person prueba = new Person("Verbo",3506163,122,Gender.MALE,true,false);
@@ -55,6 +73,9 @@ public class RegistryTest {
     	Assert.assertEquals(RegisterResult.VALID, result);
     }
     
+    /**
+     * La prueba debe debe dar valido ya que su edad es 121 
+     */
     @Test
     public void RangoEdadMayorAceptadaAnterior() {
     	Person prueba = new Person("Yeison",65461343,121,Gender.MALE,true,false);
@@ -62,6 +83,9 @@ public class RegistryTest {
     	Assert.assertEquals(RegisterResult.VALID, result);
     }
     
+    /**
+     * La prueba debe dar que la persona es menor de edad porque es 1
+     */
     @Test
     public void RangoEdadPositiva() {
     	Person prueba = new Person("Yesid",55461343,1,Gender.MALE,true,false);
@@ -69,6 +93,9 @@ public class RegistryTest {
     	Assert.assertEquals(RegisterResult.UNDERAGE, result);
     }
     
+    /**
+     * La prueba debe que la edad es invalida ya que es de 0
+     */
     @Test
     public void edadZero() {
     	Person prueba = new Person("Manuel",45461343,0,Gender.MALE,true,false);
@@ -76,6 +103,9 @@ public class RegistryTest {
     	Assert.assertEquals(RegisterResult.INVALID_AGE, result);
     }
     
+    /**
+     * La prueba debe dar edad invalida porque es -1
+     */
     @Test
     public void RangoEdadNegativa() {
     	Person prueba = new Person("Sebastian",25461343,-1,Gender.MALE,true,false);
@@ -83,10 +113,12 @@ public class RegistryTest {
     	Assert.assertEquals(RegisterResult.INVALID_AGE, result);
     }
     
-    
+    /**
+     * la prueba debe dar que esta muerto porque es falso el parametro
+     */
     @Test
     public void isNotAlive() {
-    	Person prueba = new Person("Henry",154616,25,Gender.MALE,false,false);
+    	Person prueba = new Person("Einstein",154616,25,Gender.MALE,false,false);
     	 RegisterResult result = registry.registerVoter(prueba);
     	Assert.assertEquals(RegisterResult.DEAD, result);
     }
